022ca24390825be3db44b119d90970fac4e2e49f
Adepoju oluwasegun <oluwasegunadepoju@gmail.com>
2021-06-20 23:25:53 +0100

    ft: update hoome orientation

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -6,14 +6,29 @@ const Home: React.FC = (): ReactElement => {
   return (
     <div className="main_container">
       <NavigationBar />
-      <div className="body_container">
-        <div className="centered_description">
+      {/* <section className="centered_description">
           <div className="info">
             <div className="name">Olúwáségun</div>
             <div className="title">Software Engineer</div>
           </div>
-        </div>
+        </section> */}
+      <div className="bodySection">
+        <section className="section_container">
+          <div className="section_colomn_right">
+            <div className="name">Olúwáségun</div>
+            <div className="title">Software Engineer</div>
+          </div>
+          <div className="section_colomn_center info">
+            <div className="name">Olúwáségun</div>
+            <div className="title">Software Engineer</div>
+          </div>
+          <div className="section_colomn_left">
+            <div className="name">Olúwáségun</div>
+            <div className="title">Software Engineer</div>
+          </div>
+        </section>
       </div>
+
       <Footer />
     </div>
   );
