db19716e1932a29e8d82c4e641a1550236a3a2d2
Adepoju oluwasegun <oluwasegunadepoju@gmail.com>
2022-10-04 06:02:04 +0200

    update typings

--- src/utils/apiRequest.ts
+++ src/utils/apiRequest.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -16,27 +16,27 @@ const token: string = process.env.FAKEJSONTOKEN || 'Xd00zQlISp8G7xcmERPu2A';
 
 export const Get = async (route: string) => {
   try {
-    const response = await request.get(route);
+    const response: AxiosResponse = await request.get(route);
     return response.data;
-  } catch (error) {
+  } catch (error: any) {
     return error.response ? error.response.data : error;
   }
 };
 
 export const Post = async (data: Record<string, unknown>, route = '') => {
   try {
-    const response = await request.post(route, { token, data });
+    const response: AxiosResponse = await request.post(route, { token, data });
     return response.data;
-  } catch (error) {
+  } catch (error: any) {
     return error.response ? error.response.data : error;
   }
 };
 
 export const Patch = async (data: Record<string, unknown>, route = '') => {
   try {
-    const response = await request.patch(route, { token, data });
+    const response: AxiosResponse = await request.patch(route, { token, data });
     return response.data;
-  } catch (error) {
+  } catch (error: any) {
     return error.response ? error.response.data : error;
   }
 };
